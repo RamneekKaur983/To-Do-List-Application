5637a352ae300cad4ac197906f54689ad5d8859f
unknown <ramneek983@gmail.com>
2020-10-07 01:12:06 +0530

    sdbfebr

--- app.js
+++ app.js
@@ -220,7 +220,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
+
 app.listen(port , function()
 {
     console.log("Server is running on port 3000")
